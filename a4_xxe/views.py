50b3642c7b155a53e6dad585d109ae773442d02d
kezzyhko <s.semushin@innopolis.university>
2019-05-30 16:51:29 +0300

    Added default value for settings.xml

--- a4_xxe/views.py
+++ a4_xxe/views.py
@@ -7,9 +7,11 @@ from vulnexamples.forms import FileUploadForm
 from .resolvers import OnlyOneURLResolver
 
 
+DEFAULT_SIZE = 20
+DEFAULT_MINES = 50
+
+
 class IndexView(MyFormView):
-    DEFAULT_SIZE = 20
-    DEFAULT_MINES = 50
 
     template_name = 'a4_xxe/index.html'
     form_class = FileUploadForm
@@ -20,8 +22,8 @@ class IndexView(MyFormView):
             parser = XMLParser(resolve_entities=True)
             parser.resolvers.add(OnlyOneURLResolver())
             xml = etree.parse(request.FILES.get('file'), parser=parser).getroot()
-            self.mines = getattr(xml.find('mines'), "text", self.DEFAULT_MINES)
-            self.size = getattr(xml.find('size'), "text", self.DEFAULT_SIZE)
+            self.mines = getattr(xml.find('mines'), "text", DEFAULT_MINES)
+            self.size = getattr(xml.find('size'), "text", DEFAULT_SIZE)
         except ParseError as e:
             self.form.errors['file'] += [str(e)]
 
@@ -31,9 +33,13 @@ class IndexView(MyFormView):
     def render_form(self, request):
         return render(request, self.template_name,
                       {'form': self.form,
-                       'mines': getattr(self, 'mines', self.DEFAULT_MINES),
-                       'size': getattr(self, 'size', self.DEFAULT_SIZE)})
+                       'mines': getattr(self, 'mines', DEFAULT_MINES),
+                       'size': getattr(self, 'size', DEFAULT_SIZE)})
 
 
 def settings(request):
-    return render(request, 'a4_xxe/settings.xml', content_type='text/xml')
+
+    return render(request, 'a4_xxe/settings.xml',
+                  {'size': request.GET.get('size', DEFAULT_SIZE),
+                   'mines': request.GET.get('mines', DEFAULT_MINES)},
+                  content_type='text/xml')
